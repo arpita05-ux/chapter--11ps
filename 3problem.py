217105fa9e54e2add035e6f646113d4bd7219e7d
“Arpita <arpitakumari10653@gmail.com>
2025-07-04 21:10:56 +0530

    Write a method ‘salaryAfterIncrement’ method with a @property decorator with a setter which changes the value of increment based on the salary.

--- 3problem.py
+++ 3problem.py
@@ -1,5 +1,18 @@
 class Employee:
     salary = 23456
-    increment = 2980
+    increment = 100
+
+
+    @property
+    def salaryAfterIncrement(self):
+        return(self.salary + self.salary * (self.increment/100))
+    
+    @salaryAfterIncrement.setter
+    def salaryAfterIncrement(self, salary):
+        self.increment = ((salary/self.salary)-1)*100
+    
 
 e = Employee()    
+# print(e.salaryAfterIncrement)
+e.salaryAfterIncrement = 46982.0
+print(e.increment)  
